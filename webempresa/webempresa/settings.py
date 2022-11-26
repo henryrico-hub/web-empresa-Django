fa859d6f8ed065889a8c20186cc308520a051c0b
Henry Rico <henry.ricoe@gmail.com>
2022-11-26 00:22:01 -0700

    change for deploy

--- webempresa/webempresa/settings.py
+++ webempresa/webempresa/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-emao#s1f%_n6p0x3j%2@2=f+o%lvv+e)xdbl&&b1(hqcg9nq#8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = int(os.environ.get("DEBUG", default=1))
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0', 'localhost', '127.0.0.1', 'llantasmerida.com']
 
 
 # Application definition
