d41b14e4a5aa39f325f67417ea044f6580b08ce5
Henry Rico <henry.ricoe@gmail.com>
2022-11-30 18:17:34 -0700

    deploy try

--- webempresa/webempresa/settings.py
+++ webempresa/webempresa/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-emao#s1f%_n6p0x3j%2@2=f+o%lvv+e)xdbl&&b1(hqcg9nq#8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = int(os.environ.get("DEBUG", default=1))
+DEBUG = True
 
-ALLOWED_HOSTS = ['0.0.0.0', 'localhost', '127.0.0.1', 'llantasmerida.com']
+ALLOWED_HOSTS = ['167.99.15.185','0.0.0.0', 'localhost', '127.0.0.1', 'llantasmerida.com']
 
 
 # Application definition
@@ -121,6 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
